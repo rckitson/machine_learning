corrected gradient calculation

optimizers/optimizer.py
@@ -18,7 +18,9 @@ class Optimizer:
         Args:
             x0: The initial point
             function: The function to optimize
-            method: The optimization method 
+            method: The optimization method, optional
+            error_threshold: The error threshold, optional
+            learning_rate: The learning rate, optional
         """
         self.x0 = x0
         self.function = function
@@ -57,7 +59,7 @@ class Optimizer:
 
         value0 = value
         timeout = False
-        while np.linalg.norm(gradient) > self.error_threshold:
+        while np.linalg.norm(value) > self.error_threshold:
             count += 1
             value, gradient = self.evaluate_function(next_point)
             next_point = self.method(next_point, gradient, count)
@@ -85,11 +87,13 @@ class Optimizer:
             A tuple: (function value, gradient) 
         """
 
-        rand = np.random.random(len(x))
-        delta = 1e-6 * rand / np.linalg.norm(rand)
-
         value = self.function(x)
-        gradient = (self.function(x + delta) - value) / delta
+        gradient = np.zeros_like(x)
+        for ii in range(len(gradient)):
+            delta = np.zeros_like(x)
+            step = 1e-12
+            delta[ii] = step
+            gradient[ii] = (self.function(x + delta) - value) / step
         return value, gradient
 
     def sgd(self, x, gradient, learning_rate):

optimizers/test_optimizer.py
@@ -7,21 +7,29 @@ https://en.wikipedia.org/wiki/Test_functions_for_optimization
 import os
 import glob
 import shutil
+import subprocess
 import numpy as np
 from matplotlib import pyplot as plt
 import optimizer
 
-np.random.seed(0)
-for ff in glob.glob('*.dat'):
-    os.remove(ff)
 
-TOLERANCE = 1e-3
-LEARNING_RATE = 5e-3
-# See the first few methods for available test functions
-# This can be increased to higher dimensions
-X0 = np.random.random(1)
-X0 = X0 / np.linalg.norm(X0)
-print(X0)
+def main():
+    """ The main routine """
+    np.random.seed(0)
+    for ff in glob.glob('*.dat'):
+        os.remove(ff)
+
+    tolerance = 1e-6
+    learning_rate = 1e-4
+    function = circle
+    # See the first few methods for available test functions
+    # x0 can be increased to higher dimensions
+    x0 = np.random.random(3)
+    x0 = x0 / np.linalg.norm(x0)
+
+    test_all(x0, function, learning_rate, tolerance)
+    plot_history()
+    subprocess.call(['open', 'convergence_history.png'])
 
 
 def rosenbrock(x):
@@ -39,10 +47,19 @@ def circle(x):
     return np.sum(x ** 2)
 
 
-def test_all():
+def test_all(x0, function, learning_rate, tolerance):
+    """ Test all the algorithms
+
+    Args:
+        x0: The initial point
+        function: The test function
+        learning_rate: The learning rate
+        tolerance: The tolerance on the gradient
+
+    """
     for algorithm in ['sgd', 'momentum', 'adam', 'rmsprop', 'adagrad', 'adadelta']:
-        opt = optimizer.Optimizer(x0=X0, function=circle, method=algorithm,
-                                  learning_rate=LEARNING_RATE, error_threshold=TOLERANCE)
+        opt = optimizer.Optimizer(x0=x0, function=function, method=algorithm,
+                                  learning_rate=learning_rate, error_threshold=tolerance)
         opt.solve()
         shutil.move('history.dat', algorithm + '.dat')
 
@@ -71,5 +88,4 @@ def plot_history():
 
 
 if __name__ == "__main__":
-    test_all()
-    plot_history()
+    main()